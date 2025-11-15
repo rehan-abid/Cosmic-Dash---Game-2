59be5d4b4323e597aee341b3deba492a3a64b8a7
rehan-abid <mrehanabid1@gmail.com>
2025-11-15 17:04:18 +0500

    SAT 15 NOV ( Player and Background)
    1-Making the Frames Move and SwitchBack
    2-Added Colliders for floor
    3-added the Astronaut
    4-Added Collider on Astraunaut

--- MarsGame/Assets/Scripts/GroundScroller.cs
+++ MarsGame/Assets/Scripts/GroundScroller.cs
@@ -1,16 +1,38 @@
 using UnityEngine;
-
 public class GroundScroller : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float scrollSpeed = 1f;
+    private float tileWidth; //Stores The Width of the Background Image to switch Back after some interval
+                             // Start is called before the first frame update               
     void Start()
     {
-        
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+        if (spriteRenderer != null)
+        {
+             tileWidth = spriteRenderer.bounds.size.x; 
+        }
+        else
+        {
+            Debug.LogError("The SpriteRenderer Component is Missing Form the Background Object");
+            enabled = false; //Disable the Script at Start
+        }
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector3.left * scrollSpeed * Time.deltaTime);
+        if (transform.position.x <= -tileWidth)
+        {
+            RepositionTile();
+        }
+
+    }
+
+    void RepositionTile()
+    {
+        Vector3 Offset = new Vector3(tileWidth * 2f, 0, 0);
+        transform.position += Offset;
+
     }
 }
